231fe1f5cec380e92615d0ef27ef711bf6d7ef72
MaximusGyraditus <kanisornputta@gmail.com>
2024-05-20 20:53:22 +0700

    First

---
+++ Sensor1/Sensor1.ino
@@ -0,0 +1,179 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+#include <SoftwareSerial.h>
+EspSoftwareSerial::UART StmSerial;
+int ddht = 14; //D5
+int relay = 12; //D6
+String autopump = "off";
+float AP = 0.0f;
+String data = "";
+#define DHTTYPE DHT22
+#define D0 16
+#define LED_PIN D0
+#define RELAY_PIN 12
+
+DHT dht(ddht, DHTTYPE);
+
+// WiFi Parameters
+const char* ssid = "Garfield";
+const char* password = "17215723";
+
+// NetPie Parameters
+const char* mqtt_server = "broker.netpie.io";
+const int mqtt_port = 1883;
+const char* mqtt_Client = "a9738c8a-4c31-41d3-8ef3-e98b47c291ad";
+const char* mqtt_username = "ANZt9V5EhmDaWaLQyW3B5tWuJNwvCfns";
+const char* mqtt_password = "3RQ8nJFGtCoyLyxbTQLMeaHPsGGHqEr2";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char msg[100],msg2[100];
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection…");
+    if (client.connect(mqtt_Client, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe("@msg/#");  
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String message;
+  String tpc;
+  for (int i = 0; i < length; i++) {
+    message = message + (char)payload[i];
+  }
+  Serial.println(message);
+  tpc = getMsg(topic, message);
+}
+
+void setStatus(String status) {
+  if (status == "on") {
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(RELAY_PIN, HIGH); //relay เปิด
+    autopump = "on";
+    AP = 1.0f;
+    Serial.println("ON");
+    StmSerial.write("ON");
+  }
+  else if (status == "off") {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(RELAY_PIN, LOW); //relay ปิด
+    autopump = "off";
+    AP = 0.0f;
+    Serial.println("OFF");
+  }
+}
+
+void toggleStatus() {
+  if (autopump == "on")
+    setStatus("off");
+  else 
+    setStatus("on");
+}
+
+void ReadSTM() {
+  if(StmSerial.available() > 0){
+    data = "";
+    uint8_t state=0;
+    while(StmSerial.available()){
+      char c = StmSerial.read();
+      data += c;
+    }
+    toggleStatus();
+  }
+  // Serial.println(data);
+}
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(relay, LOW); // relay ปิด
+  Serial.begin(115200);
+  StmSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false, 100, 100);
+  dht.begin();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  String status = "off";
+}
+
+void loop() {
+
+  ReadSTM();
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (autopump == "on") {
+    if (h > 80) {
+      digitalWrite(relay, LOW); //เปิด
+    } else {
+      digitalWrite(relay, HIGH); //ปิด
+    }
+  }
+
+  // Dust Density Sensor
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (3.3 / 1023.0);
+  float dustDensity = (voltage - 0.0356) / 0.000377;
+  dustDensity = max(dustDensity, 0.0f);
+
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorValue);
+  Serial.print(", Voltage: ");
+  Serial.print(voltage);
+  Serial.print(", Dust Density: ");
+  Serial.print(dustDensity);
+  Serial.println(" mg/m3");
+
+
+  float r = dustDensity; // r = Dust Percentage
+  // String data = "{\"data\":{\"Humidity\": " + String(h) + ",\"Temperature\": " + String(t) + ",\"Dust Density\": " + String(r) + "}}";
+  // String msgData =  String(t) + "," + String(h) + "," + String(r); 
+
+  String data = "{\"data\":{\"Humidity\": " + String(h) + ",\"Temperature\": " + String(t) + ",\"Dust Density\": " + String(r) + ",\"Status\": " + String(AP) + "}}";
+  String msgData =  String(t) + "," + String(h) + "," + String(r) + "," + String(AP); 
+  
+  Serial.println(data);
+  Serial.println(msgData);
+  msgData.toCharArray(msg2,(msgData.length() + 1));
+  data.toCharArray(msg , (data.length() + 1));
+  client.publish("@shadow/data/update", msg);
+  client.publish("@msg/test",msg2);
+  AP = 0.5f;
+  client.loop();
+  delay(1000);
+}
+
+String getMsg(String topic_, String message_) {     
+  if (topic_ == "@msg/on") {
+    setStatus(message_);                          //netpie["???"].publish("@msg/relay","on")
+  }
+  return autopump;
+}
---
+++ WEB/netpie.js
@@ -0,0 +1,99 @@
+var x = 0;
+
+let temperature,dustLevel
+var humidity = 50 
+
+client = new Paho.MQTT.Client("mqtt.netpie.io",443,"d88bd0e4-0a50-4c03-bd82-9aaa6c4bc94e"); // The last one is Client ID
+client.onMessageArrived = onMessageArrived;
+
+var options = {
+    useSSL: true,
+    userName: "78Rg1vZ1uGQz3yd3UkAxB3PBJnnB7gFH", // This is token
+    password: "XYcqPXeSJBJ2NWHLL1U2qe5u6JQcrbt2", // This is secret
+    onSuccess: onConnect,
+    onFailure: doFail,                                                  
+}
+
+
+
+client.connect(options);
+
+function onConnect(){
+    client.subscribe("@msg/test");
+}
+
+function doFail(e){
+    console.log(e);
+}
+
+function toggleStatus()
+{
+    // เป็นปุ่มที่ใช้เปิดปิดน้ำ
+    var button = document.getElementById("my-button");
+    if (button.innerHTML === "off") {
+        setStatus("on")
+        mqttSend("@msg/on", "on");
+    } else {
+        setStatus("off")   
+        mqttSend("@msg/on", "off");
+    }
+    console.log(button.innerHTML);
+    // onMessageArrived("")
+}
+
+function setStatus(status) {
+    var button = document.getElementById("my-button");
+    if (status == "on") {
+        button.innerHTML = "on";
+        button.classList.add("on"); // เพิ่มคลาส 'on' เพื่อเปลี่ยนสีเป็นสีเขียว
+    }
+
+    else {
+        button.innerHTML = "off";
+        button.classList.remove("on"); // ลบคลาส 'on' เพื่อกลับไปสีแดง
+    }
+}
+
+
+
+function onMessageArrived(message)
+{
+    // ใช้ array ขนาด 3 ช่อง test ก่อนก็ได้นะแบบเสมือนรับข้อมูลจาก Sensor มา ยังไม่ต้องส่งข้อมูลจริงๆ
+    // เราน่าจะรับมาเป็น String รูปแบบเป็น temp,humi,dust ละเอามา split ทำเป็น array ขนาด 3 ช่อง
+
+    // ถ้าจะเทสการรับ message ให้เปิดตรงนี้
+    // document.getElementById("show").innerHTML = message.payloadString;
+    var data = message.payloadString.split(",");
+    // var data = [];
+    // for (var i = 0; i < 3; i++) {
+    //     // Generate random number between 0 and 100
+    //     var randomNumber = Math.floor(Math.random() * 101);
+    //     data.push(randomNumber);
+    // }
+
+    document.getElementById("temp").innerHTML = data[0] + " &deg;C";
+    document.getElementById("humi").innerHTML = data[1] + " %";
+    document.getElementById("dust").innerHTML = data[2] + " mg/m3";
+
+    state = data[3];
+    if (state == 1) setStatus("on")
+    else if (state == 0) setStatus("off")
+
+    console.log(data);
+}
+
+var mqttSend = function (topic,msg){
+    var message = new Paho.MQTT.Message(msg);
+    message.destinationName = topic;
+    client.send(message);
+}
+
+
+
+
+
+
+
+
+
+
